8d2e39121a38a7287f60b6f77f08f64954682558
Anatoly Kovalev <inatoley@gmail.com>
2015-03-18 20:08:52 +0300

    yea
    keep goin

---
+++ listDaitems/TaskCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskCell.swift
+//  listDaitems
+//
+//  Created by Fenkins on 18/03/15.
+//  Copyright (c) 2015 Fenkins. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
